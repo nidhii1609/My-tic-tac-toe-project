0002d9f2aa1c932125ce6639a9692f4fc5e6f184
Nidhi <nidhimehar123@gmail.com>
2020-07-24 01:27:18 +0530

    modifing the font and the end game msg

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var origBoard;
 var huPlayer='X';
 var aiPlayer='O';
+const cells = document.querySelectorAll('.cell');
 const winCombos = [
 	[0, 1, 2],
 	[3, 4, 5],
@@ -12,7 +13,35 @@ const winCombos = [
 	[6, 4, 2]
 ]
 
-const cells = document.querySelectorAll('.cell');
+	var depth = -1;
+	var Human_symbol=1;
+	startGame(depth,Human_symbol);
+
+	document.getElementById("depth").addEventListener("click", function (event) {
+		if (event.target.tagName !== "LI" || event.target.dataset.value===-1) return;
+
+		var depth_choices = document.querySelectorAll(".depth_choices ul li");
+		depth_choices.forEach(function (choice) {
+			removeClass(choice, 'active');
+		});
+		addClass(event.target, 'active');
+		depth = event.target.dataset.value;
+	}, false);
+
+	document.getElementById("symbol").addEventListener("click", function (event) {
+
+		Human_symbol=event.target.dataset.value;
+		var symbol_choices = document.querySelectorAll(".symbol_choices ul li");
+		symbol_choices.forEach(function (choice) {
+			removeClass(choice, 'active');
+		});
+		addClass(event.target, 'active');
+
+	}, false);
+
+	document.getElementById("newgame").addEventListener('click', function () {
+		startGame(depth,Human_symbol);
+	});
 
 function hasClass(el, className) {
 	if (el.classList) return el.classList.contains(className);else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
@@ -28,7 +57,7 @@ function removeClass(el, className) {
 }
 
 function startGame(depth,Human_symbol) {
-
+	
 	document.querySelector(".endgame").style.display = "none";
 	origBoard = Array.from(Array(9).keys());
 
@@ -39,14 +68,11 @@ function startGame(depth,Human_symbol) {
 			huPlayer='O';
 			aiPlayer='X';
 	}
-	
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].innerText = '';
 		cells[i].style.removeProperty('background-color');
 		cells[i].addEventListener('click', turnClick, false);
 	}
-	
-	
 }
 
 function turnClick(square) {
@@ -81,10 +107,10 @@ function gameOver(gameWon) {
 	
 	for (let index of winCombos[gameWon.index]) {
 		document.getElementById(index).style.backgroundColor =
-		    gameWon.player == huPlayer ? " #8c080b" : " #8c080b";
+		    gameWon.player == huPlayer ? " blue" : " red";
 		    
 	}
-	declareWinner(gameWon.player == huPlayer ? "You win!" : "You lose.");
+	declareWinner(gameWon.player == huPlayer ? "Yipee.. You win!" : "You lose. Better luck next Time");
 	for (var i = 0; i < cells.length; i++) {
 		cells[i].removeEventListener('click', turnClick, false);
 	}
@@ -101,16 +127,16 @@ function emptySquares() {
 }
 
 function bestSpot() {
-	return minimax(origBoard, aiPlayer).index;
+	return minimax(origBoard, aiPlayer,window.depth).index;
 }
 
 function checkTie() {
 	if (emptySquares().length == 0) {
 		for (var i = 0; i < cells.length; i++) {
-			//cells[i].style.backgroundColor = "green";
+			cells[i].style.backgroundColor = "green";
 			cells[i].removeEventListener('click', turnClick, false);
 		}
-		declareWinner("Tie Game!");
+		declareWinner("Tie Game. Try to win next time!");
 		return true;
 	}
 	return false;
@@ -119,6 +145,7 @@ function checkTie() {
 function minimax(newBoard, player) {
 
 	var availSpots = emptySquares();
+	var my_depth=window.depth;
 
 	if (checkWin(newBoard, huPlayer)) {
 		return {score: -10};
@@ -127,6 +154,7 @@ function minimax(newBoard, player) {
 	} else if (availSpots.length === 0) {
 		return {score: 0};
 	}
+
 	var moves = [];
 	for (var i = 0; i < availSpots.length; i++) {
 		var move = {};
@@ -146,15 +174,6 @@ function minimax(newBoard, player) {
 		moves.push(move);
 	}
 
-	/*if(starting_player===0){
-		var center_and_corners = [0, 2, 4, 6, 8];
-		var first_choice = center_and_corners[Math.floor(Math.random() * center_and_corners.length)];
-		//cells[first_choice].innerText = aiPlayer;
-		starting_player=1;
-		return cells[first_choice];
-	}
-*/
-
 	var bestMove;
 	if(player === aiPlayer) {
 		var bestScore = -10000;
@@ -165,16 +184,23 @@ function minimax(newBoard, player) {
 					bestMove = i;
 				}
 			}
-		/*if(depth===-1){
+
+		if(my_depth==-1){
 			return moves[bestMove];
-		}else if(depth===1){
-			
+		}
+		if(my_depth==1){
+			var random=moves[Math.floor(Math.random()*moves.length)];
+			return random;
 
-		}else if(depth===2){
+		}
+		/*if(depth==2){
 
-		}else if(depth===3){
 
-		}else if(depth===4){
+		}else if(depth==3){
+
+
+		}else if(depth==4){
+
 
 		}*/
 	} else {
@@ -190,8 +216,7 @@ function minimax(newBoard, player) {
 	return moves[bestMove];
 }
 
-document.addEventListener("DOMContentLoaded", function (event) {
-
+/*document.addEventListener("DOMContentLoaded", function (event) {
 	
 	var depth = -1;
 	var Human_symbol=1;
@@ -201,7 +226,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		if (event.target.tagName !== "LI" || event.target.dataset.value===-1) return;
 
 		var depth_choices = document.querySelectorAll(".depth_choices ul li");
-		console.log(depth_choices);
 		depth_choices.forEach(function (choice) {
 			removeClass(choice, 'active');
 		});
@@ -220,9 +244,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 	}, false);
 
-
 	document.getElementById("newgame").addEventListener('click', function () {
 		startGame(depth,Human_symbol);
 	});
 
-});
+});*/
